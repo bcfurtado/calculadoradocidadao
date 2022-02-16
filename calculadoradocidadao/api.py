2e32af9c71c24855eee8e28c4120e623a0323de2
Bruno Furtado <brunofurtadoc@gmail.com>
2022-02-16 01:44:08 +0100

    Extract api gateway from api view

---
+++ calculadoradocidadao/api.py
@@ -0,0 +1,25 @@
+import lxml.html
+
+import requests
+
+
+BASE_URL = 'https://www3.bcb.gov.br/CALCIDADAO/publico'
+CORRIGIR_PELA_SELIC_URL = f'{BASE_URL}/corrigirPelaSelic.do?method=corrigirPelaSelic'
+
+
+class CalculadoraDoCidadaoAPI:
+    def get_valor_corrigido(self, data_inicial, data_final, valor_correcao):
+        values = {
+            'dataInicial': data_inicial,
+            'dataFinal': data_final,
+            'valorCorrecao': valor_correcao,
+        }
+
+        response = requests.post(CORRIGIR_PELA_SELIC_URL, data=values)
+
+        return self._parse_response(response.text)
+
+    def _parse_response(self, response):
+        root = lxml.html.fromstring(response)
+        result = root.xpath('//form/div[2]/table/tbody/tr[8]/td[2]/text()')
+        return result[0]
--- calculadoradocidadao/views.py
+++ calculadoradocidadao/views.py
@@ -2,7 +2,8 @@ import lxml.html
 
 from flask import request
 from flask_restful import Resource
-import requests
+
+from calculadoradocidadao.api import CalculadoraDoCidadaoAPI
 
 
 class HomeView(Resource):
@@ -11,31 +12,20 @@ class HomeView(Resource):
 
 
 class CorrigirPelaSelicView(Resource):
+    api = CalculadoraDoCidadaoAPI()
+
     def post(self):
         form = request.form
-        response = self.get_valor_corrigido(
-            form['dataInicial'], form['dataFinal'], form['valorCorrecao']
+
+        valor_corrigido = self.api.get_valor_corrigido(
+            form['dataInicial'],
+            form['dataFinal'],
+            form['valorCorrecao'],
         )
-        valorCorrigido = self.parse_response(response.text)
 
         return {
             'dataInicial': form['dataInicial'],
             'dataFinal': form['dataFinal'],
             'valorCorrecao': form['valorCorrecao'],
-            'valorCorrigido': valorCorrigido,
+            'valorCorrigido': valor_corrigido,
         }
-
-    def get_valor_corrigido(self, data_inicial, data_final, valor_correcao):
-        url = 'https://www3.bcb.gov.br/CALCIDADAO/publico/corrigirPelaSelic.do?method=corrigirPelaSelic'
-        values = {
-            'dataInicial': data_inicial,
-            'dataFinal': data_final,
-            'valorCorrecao': valor_correcao,
-        }
-
-        return requests.post(url, data=values)
-
-    def parse_response(self, response):
-        root = lxml.html.fromstring(response)
-        result = root.xpath('//form/div[2]/table/tbody/tr[8]/td[2]/text()')
-        return result[0]
